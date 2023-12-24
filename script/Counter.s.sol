ff2314c32206590cbc77f8781ae30bd897b9b85b
parwejkhan1816 <pkhan181611786@gmail.com>
2023-12-24 18:32:40 +0530

    first commit

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -3,10 +3,17 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 
+import  {Counter} from "../src/Counter.sol" ; 
+
 contract CounterScript is Script {
     function setUp() public {}
 
-    function run() public {
-        vm.broadcast();
+    function run() external returns(Counter counter) {
+        vm.startBroadcast() ; 
+
+        Counter counter  = new Counter() ; 
+
+        vm.stopBroadcast() ; 
+        return counter ; 
     }
 }
